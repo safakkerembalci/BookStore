d2d20cf08fbeef7d5ba5d39559f0ea66846b84d1
Şafak Kerem Balcı <104922046+safakkerembalci@users.noreply.github.com>
2022-11-30 03:45:28 +0300

    Add files via upload

---
+++ BookStore with Model Validation/WebApi/Program.cs
@@ -0,0 +1,40 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using WebApi.DBOperations;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseInMemoryDatabase(databaseName:"BookStoreDB"));
+        builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        using (var scope = app.Services.CreateScope()) { var services = scope.ServiceProvider; DataGenerator.Initialize(services); }
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
