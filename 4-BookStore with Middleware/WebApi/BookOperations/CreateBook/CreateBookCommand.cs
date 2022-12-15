09e5edc0fe0a44abb15f3bd74e3456be83e5e2b3
Şafak Kerem Balcı <104922046+safakkerembalci@users.noreply.github.com>
2022-12-15 15:22:23 +0300

    Add files via upload

---
+++ 4-BookStore with Middleware/WebApi/BookOperations/CreateBook/CreateBookCommand.cs
@@ -0,0 +1,37 @@
+using WebApi.DBOperations;
+using AutoMapper;
+
+namespace WebApi.BookOperations.CreateBook
+{
+    public class CreateBookCommand
+    {
+        public CreateBookModel Model { get; set; }
+        private readonly BookStoreDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public CreateBookCommand(BookStoreDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public void Handle()
+        {
+            var book = _dbContext.Books.SingleOrDefault(x => x.Title == Model.Title);
+            if (book is not null)
+                throw new InvalidOperationException("The book already exists!");
+            book = _mapper.Map<Book>(Model);
+
+            _dbContext.Books.Add(book);
+            _dbContext.SaveChanges();
+        }
+
+        public class CreateBookModel
+        {
+            public string? Title { get; set; }
+            public int PageCount { get; set; }
+            public DateTime PublishDate { get; set; }
+            public int GenreId { get; set; }
+        }
+    }
+}
